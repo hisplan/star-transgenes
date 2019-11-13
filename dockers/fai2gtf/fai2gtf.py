d6428f2af90fddc664fbae099f3ed857ca3bcb52
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-11-13 12:28:31 -0500

    Split to gene ID and transcript ID

--- dockers/fai2gtf/fai2gtf.py
+++ dockers/fai2gtf/fai2gtf.py
@@ -4,7 +4,30 @@ import sys
 import argparse
 
 
-def main(path_faidx, ensembl_id):
+
+def add_line(feature, gene_name, seq_length, gene_id, transcript_id):
+
+    if feature not in ["gene", "transcript", "exon"]:
+        raise Exception("Invalid feature!")
+
+    # fixme: use gtf writer instead
+    if feature == "gene":
+        comment = 'gene_id "{0}"; gene_name "{1}";'.format(
+            gene_id, gene_name
+        )
+    else:
+        comment = 'gene_id "{0}"; transcript_id "{1}"; gene_name "{2}";'.format(
+            gene_id, transcript_id, gene_name
+        )
+
+    out = [
+        gene_name, "TRANSGENE", feature, 1, seq_length, ".", "+", ".", comment
+    ]
+
+    return "\t".join(map(str, out))
+
+
+def main(path_faidx, gene_id, transcript_id):
 
     with open(path_faidx, "rt") as fo:
 
@@ -23,24 +46,17 @@ def main(path_faidx, ensembl_id):
             seq_length = columns[1]
 
             # https://useast.ensembl.org/info/website/upload/gff.html
-            # tdTomato  TRANSGENE  gene        1	1431	.	+	.	gene_id "ENSMUSG09000000002"; transcript_id "ENSMUSG09000000002"; gene_name "tdTomato";
-            # tdTomato  TRANSGENE  transcript  1	1431	.	+	.	gene_id "ENSMUSG09000000002"; transcript_id "ENSMUSG09000000002"; gene_name "tdTomato";
-            # tdTomato  TRANSGENE  exon        1	1431	.	+	.	gene_id "ENSMUSG09000000002"; transcript_id "ENSMUSG09000000002"; gene_name "tdTomato";
+            # tdTomato  TRANSGENE  gene        1	1431	.	+	.	gene_id "ENSMUSG09000000002"; gene_name "tdTomato";
+            # tdTomato  TRANSGENE  transcript  1	1431	.	+	.	gene_id "ENSMUSG09000000002"; transcript_id "ENSMUST09000000002"; gene_name "tdTomato";
+            # tdTomato  TRANSGENE  exon        1	1431	.	+	.	gene_id "ENSMUSG09000000002"; transcript_id "ENSMUST09000000002"; gene_name "tdTomato";
 
-            # fixme: use gtf writer instead
-            comment = 'gene_id "{0}"; transcript_id "{0}"; gene_name "{1}";'.format(
-                ensembl_id, gene_name
-            )
+            gene_line = add_line("gene", gene_name, seq_length, gene_id, None)
+            transcript_line = add_line("transcript", gene_name, seq_length, gene_id, transcript_id)
+            exon_line = add_line("exon", gene_name, seq_length, gene_id, transcript_id)
 
-            out = [
-                gene_name, "TRANSGENE", "gene", 1, seq_length, ".", "+", ".", comment
-            ]
-
-            print("\t".join(map(str, out)))
-            out[2] = "transcript"
-            print("\t".join(map(str, out)))
-            out[2] = "exon"
-            print("\t".join(map(str, out)))
+            print(gene_line)
+            print(transcript_line)
+            print(exon_line)
 
 
 def parse_arguments():
@@ -57,10 +73,18 @@ def parse_arguments():
 
     # https://useast.ensembl.org/info/genome/stable_ids/index.html
     parser.add_argument(
-        "--ensembl-id",
+        "--gene-id",
         action="store",
-        dest="ensembl_id",
-        help="Ensenmbl ID",
+        dest="gene_id",
+        help="Ensenmbl Gene ID (e.g. ENSMUSG00000900001)",
+        required=True
+    )
+
+    parser.add_argument(
+        "--transcript-id",
+        action="store",
+        dest="transcript_id",
+        help="Ensenmbl Transcript ID (e.g. ENSMUST00000900001)",
         required=True
     )
 
@@ -76,5 +100,6 @@ if __name__ == "__main__":
 
     main(
         params.path_faidx,
-        params.ensembl_id
+        params.gene_id,
+        params.transcript_id
     )
