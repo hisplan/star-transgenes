ec7183807753af4cc3ae18605819bd6257414a95
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2020-10-20 15:39:44 -0400

    Remove unnecessary (will use the one in the docker-star repo)

--- dockers/STAR/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t cromwell-star:2.5.3a .
--- dockers/STAR/push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker login
-docker tag cromwell-star:2.5.3a hisplan/cromwell-star:2.5.3a
-docker push hisplan/cromwell-star:2.5.3a
