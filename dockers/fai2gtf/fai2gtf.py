ce05c22ee64865a1c4c41b0b947ec557a6fa5f9e
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-07-17 06:56:42 -0400

    Write gene_biotype to GTF

--- dockers/fai2gtf/fai2gtf.py
+++ dockers/fai2gtf/fai2gtf.py
@@ -3,25 +3,29 @@
 import sys
 import argparse
 
-
-
 def add_line(feature, gene_name, seq_length, gene_id, transcript_id):
 
     if feature not in ["gene", "transcript", "exon"]:
         raise Exception("Invalid feature!")
 
+    # Ensembl uses the attribute name `gene_biotype` whereas GENCODE uses `gene_type`.
+    # here, we'll just use `gene_biotype`.
+
+    # attributes:
+    # construct a semicolon-separated list of tag-value pairs
+    # providing additional information about each feature.
     # fixme: use gtf writer instead
     if feature == "gene":
-        comment = 'gene_id "{0}"; gene_name "{1}";'.format(
+        attributes = 'gene_id "{0}"; gene_name "{1}"; gene_biotype "transgene";'.format(
             gene_id, gene_name
         )
     else:
-        comment = 'gene_id "{0}"; transcript_id "{1}"; gene_name "{2}";'.format(
+        attributes = 'gene_id "{0}"; transcript_id "{1}"; gene_name "{2}"; gene_biotype "transgene";'.format(
             gene_id, transcript_id, gene_name
         )
 
     out = [
-        gene_name, "TRANSGENE", feature, 1, seq_length, ".", "+", ".", comment
+        gene_name, "TRANSGENE", feature, 1, seq_length, ".", "+", ".", attributes
     ]
 
     return "\t".join(map(str, out))
@@ -46,9 +50,9 @@ def main(path_faidx, gene_id, transcript_id):
             seq_length = columns[1]
 
             # https://useast.ensembl.org/info/website/upload/gff.html
-            # tdTomato  TRANSGENE  gene        1	1431	.	+	.	gene_id "ENSMUSG09000000002"; gene_name "tdTomato";
-            # tdTomato  TRANSGENE  transcript  1	1431	.	+	.	gene_id "ENSMUSG09000000002"; transcript_id "ENSMUST09000000002"; gene_name "tdTomato";
-            # tdTomato  TRANSGENE  exon        1	1431	.	+	.	gene_id "ENSMUSG09000000002"; transcript_id "ENSMUST09000000002"; gene_name "tdTomato";
+            # tdTomato  TRANSGENE  gene        1	1431	.	+	.	gene_id "ENSMUSG09000000002"; gene_name "tdTomato"; gene_biotype "transgene";
+            # tdTomato  TRANSGENE  transcript  1	1431	.	+	.	gene_id "ENSMUSG09000000002"; transcript_id "ENSMUST09000000002"; gene_name "tdTomato"; gene_biotype "transgene";
+            # tdTomato  TRANSGENE  exon        1	1431	.	+	.	gene_id "ENSMUSG09000000002"; transcript_id "ENSMUST09000000002"; gene_name "tdTomato"; gene_biotype "transgene";
 
             gene_line = add_line("gene", gene_name, seq_length, gene_id, None)
             transcript_line = add_line("transcript", gene_name, seq_length, gene_id, transcript_id)
